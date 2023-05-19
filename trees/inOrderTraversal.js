3bd3d452b369961112993fe5ebae8466ead703fe
Vinit Raut <vinit.coder@gmail.com>
2023-05-19 23:46:47 +0530

    Add: In order traversal recursive solution

--- trees/inOrderTraversal.js
+++ trees/inOrderTraversal.js
@@ -4,19 +4,20 @@ const TreeNode = require('./TreeNode');
 // Recursive solution
 // Time complexity - O(N)
 // Space complexity - O(N) (Auxiallary space depends on height of the tree)
-function preOrderTraversal(root) {
+function inOrderTraversal(root) {
   if (root === null) return root;
 
+  inOrderTraversal(root.left);
   console.log(root.val);
-  preOrderTraversal(root.left);
-  preOrderTraversal(root.right);
+  inOrderTraversal(root.right);
 }
 
 // TODO: Iterative solution
-function preOrderTraversalIterative(root) {}
+function inOrderTraversalIterative(root) {}
 
 const root = new TreeNode(10);
 root.left = new TreeNode(20);
 root.right = new TreeNode(30);
 
-preOrderTraversal(root);
+// printTree(root);
+inOrderTraversal(root);
